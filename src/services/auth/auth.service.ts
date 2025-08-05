33d1106c7855f4c1b6e06262456b0c55b2677285
ega-erinovian <erinovian69@gmail.com>
2025-08-05 11:01:01 +0700

    feat: user register

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { User } from "@prisma/client";
 import { prisma } from "../../lib/prisma";
-import { comparePassword } from "../../lib/argon";
+import { comparePassword, hashPassword } from "../../lib/argon";
 import { sign } from "jsonwebtoken";
 import { JWT_SECRET } from "../../config";
 
@@ -17,9 +17,9 @@ export const loginServices = async (body: Body) => {
       throw new Error("Invalid credentials");
     }
 
-    // const isPasswordValid = await comparePassword(password, user.password);
+    const isPasswordValid = await comparePassword(password, user.password);
 
-    if (password !== user.password) {
+    if (!isPasswordValid) {
       throw new Error("Invalid credentials");
     }
 
@@ -28,6 +28,31 @@ export const loginServices = async (body: Body) => {
     const token = sign({ id: user.id }, JWT_SECRET!, { expiresIn: "2h" });
 
     return { ...userWithoutPassword, token };
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const registerServices = async (body: User) => {
+  try {
+    const { email, password } = body;
+
+    const existingUser = await prisma.user.findFirst({
+      where: { email },
+    });
+
+    if (existingUser) {
+      throw new Error("Account exist");
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    return await prisma.user.create({
+      data: {
+        ...body,
+        password: hashedPassword,
+      },
+    });
   } catch (error) {
     throw error;
   }
