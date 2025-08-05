792ff6adb4e228afb4b79c7864185b6e870d6ade
Ega Erinovian <72180780+ega-erinovian@users.noreply.github.com>
2025-08-05 11:02:14 +0700

    feat: user register (#3)

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { loginServices } from "../services/auth/auth.service";
+import { loginServices, registerServices } from "../services/auth/auth.service";
 
 export const loginController = async (
   req: Request,
@@ -8,6 +8,19 @@ export const loginController = async (
 ) => {
   try {
     const result = await loginServices(req.body);
+    res.status(200).send(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const registerController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const result = await registerServices(req.body);
     res.status(200).send(result);
   } catch (error) {
     next(error);
